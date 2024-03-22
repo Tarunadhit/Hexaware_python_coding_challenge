1e38e474af40622a4f62baccd6b3c5776cd82b98
Tarunadhit M <110289940+Tarunadhit@users.noreply.github.com>
2024-03-22 14:34:11 +0530

    Update propertyUtil.py

--- util/propertyUtil.py
+++ util/propertyUtil.py
@@ -5,6 +5,6 @@ class PropertyUtil:
     @staticmethod
     def getPropertyString():
         server_name = "DESKTOP-SRA8EHN"
-        database_name = "hospitalmanagement"
+        database_name = "hospital"
         trusted_connection = "yes"
         return f"Driver={{SQL Server}};Server={server_name};Database={database_name};Trusted_Connection={trusted_connection};"
